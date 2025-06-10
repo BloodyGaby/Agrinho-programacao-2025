c7a27fddff78634a531f14c91282d2c0e39dd211
GabriellyDrosda <141978761+BloodyGaby@users.noreply.github.com>
2025-06-10 20:34:46 -0300

    .

--- vlibras-init.js
+++ vlibras-init.js
@@ -1,3 +1,1 @@
-<><script src="https://vlibras.gov.br/app/vlibras-widget.js"></script><script>
-    new window.VLibras.Widget('https://vlibras.gov.br/app');
-</script></>
+new window.VLibras.Widget('https://vlibras.gov.br/app');
